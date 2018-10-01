eb76f66e3b4b0d3ae292f157a74afab8a991b8d9
BluEyedTree <Thomas.Bekman12@ncf.edu>
2018-10-01 00:07:22 -0600

    Added parameter 3 to the password sorter

--- Password_Sorting/Password_Scoring.py
+++ Password_Sorting/Password_Scoring.py
@@ -1,4 +1,19 @@
 
+import pymongo
+from pymongo import MongoClient
+
+
+#MongoDB information
+SUBSTRING_DATABASE = "test"
+SUBSTRING_COLECTION = "test"
+
+ASSOCIATION_RULES_DATABASE = "Association_Rules"
+ASSOCIATION_RULES_COLLECTION = "Association_Rules_half_data_0.1_Confidence"
+
+#MongoDB objects
+client = MongoClient('localhost', 27017)
+
+
 '''
 Scoring based on length
 Password length, L: normalize to 10 for example
@@ -45,11 +60,8 @@ Output score
 
 
 def common_substring_coverage(password):
-    import pymongo
-    from pymongo import MongoClient
-    client = MongoClient('localhost', 27017)
-    db = client["test"]
-    collection = db["test"]
+    db = client[SUBSTRING_DATABASE]
+    collection = db[SUBSTRING_COLECTION]
 
     password_length = len(password)
     substringList = []
@@ -72,9 +84,49 @@ def common_substring_coverage(password):
 
 
 
+'''
+parameter three P3: length of uncovered by association rules (UAL)
+In general, UL/L
+P3 = 1.4^(UAL/L * 10)
+
+
+lovehate
+iloveyou
+
+*This iterates through all mined association rules.
+
+Justification: These two passwords have 0 uncommon substrings, however, love567 is a better password than iloveyou, because ilove and you cooccur in many passwords.
+'''
+def association_rule_coverage(password):
+    db = client[ASSOCIATION_RULES_DATABASE]
+    collection = db[ASSOCIATION_RULES_COLLECTION]
+
+    association_rule_list = []
+    for obj in collection.find():
+        association_rule = obj["_id"]
+        first_word = association_rule.split("->")[0]
+        second_word = association_rule.split("->")[1]
+        if first_word in password and second_word in password:
+            association_rule_list.append((first_word,second_word))
+
+    #Both association rules need to be in the password. And if the association rule
+    password_to_modify = password
+    for first_word, second_word in association_rule_list:
+        if (first_word in password_to_modify  and second_word in password_to_modify and password_to_modify.find(first_word) < password_to_modify.find(second_word)):
+            password_to_modify = password_to_modify.replace(first_word, "")
+            password_to_modify = password_to_modify.replace(second_word, "")
+
+
+    uncovered_by_association_rules = len(password_to_modify)
+    print(1.4**(uncovered_by_association_rules/len(password) * 10))
+
+
+
 
 
 
 #Coverage 2, length 4
 print(password_score_based_on_length("castr"))
 print(common_substring_coverage("castr"))
+
+association_rule_coverage("dancseexy")
