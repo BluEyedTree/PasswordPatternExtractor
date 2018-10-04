2e1abbdee96352cdcea55f20a6377bcd34ff5d77
BluEyedTree <Thomas.Bekman12@ncf.edu>
2018-10-04 14:02:46 -0600

    Added Utility class to take the pickled score list and dump it to a text file

---
+++ Password_Sorting/dumpSortedPickleToTexrt.py
@@ -0,0 +1,9 @@
+import pickle
+with open("scoress.txt", "w+") as infile:
+    f = open('scores.pkl', 'rb')  # 'r' for reading; can be omitted
+    value_list = pickle.load(f)  # load file content as mydict
+
+    totalList = sorted(value_list, key=lambda x: x[0], reverse=True)
+    #print(totalList)
+    for item in totalList:
+        infile.write(str(item)+"\n")
