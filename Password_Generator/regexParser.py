2955bceea3eee7882327fa9271a9a7f590056f6d
BluEyedTree <Thomas.Bekman12@ncf.edu>
2018-11-01 13:51:17 -0600

    Finished regexParsing

--- Password_Generator/regexParser.py
+++ Password_Generator/regexParser.py
@@ -3,48 +3,36 @@ import re
 
 testRegex = "[a-z]{3}[0-9]{3}[a-z]{4}[0-9]{3}[a-z]{3}"
 
-regexlList = []
-for i in testRegex.split("{"):
-    for j in i.split("}"):
-        regexlList.append(j)
 
-regexlList = regexlList[:-1]
-print(regexlList)
+def breakUPRegex_(breakUpNumber):
+    regexlList = []
+    for i in testRegex.split("{"):
+        for j in i.split("}"):
+            regexlList.append(j)
 
-
-
-
-formattedRegex = ""
-for i in range(0,len(regexlList)-1,2):
-    builtRegexToAdd = regexlList[i] + "{1}"
-    formattedRegex += builtRegexToAdd*int(regexlList[i+1])
-
-breakUpNumber = 0
-count = 0
-toAdd = []
-#TODO: Write code to lump on the regex into char's of 3, and return that
-'''
-for i in formattedRegex.split("{1}"): #Get the items without the number
-
-    if(breakUpNumber == breakUpNumber)
-
-    count += 1
-'''
+    regexlList = regexlList[:-1]
+    print(regexlList)
 
 
-print(formattedRegex.split("{1}"))
 
-'''
-class RegexParser():
-    # Rocket simulates a rocket ship for a game,
-    #  or a physics simulation.
 
-    def __init__(self):
-        # Each rocket has an (x,y) position.
-        self.x = 0
-        self.y = 0
+    formattedRegex = ""
+    for i in range(0,len(regexlList)-1,2):
+        builtRegexToAdd = regexlList[i] + "{1}"
+        formattedRegex += builtRegexToAdd*int(regexlList[i+1])
 
-    def move_up(self):
-        # Increment the y-position of the rocket.
-        self.y += 1
-'''
+    breakUpNumber = 3
+    count = 0
+    toAdd = []
+
+    toBuild = ""
+    for i in formattedRegex.split("{1}"): #Get the items without the number
+        count += 1
+        toBuild += i+"{1}"
+
+        if(count == breakUpNumber):
+            toAdd.append(toBuild)
+            toBuild = ""
+            count =0
+
+    return (toAdd)
